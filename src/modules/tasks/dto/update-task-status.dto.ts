4a405fd1914bf2c22082600ff3d04a029545d8c2
HongAnTran <tranhongankrn.2001@gmail.com>
2025-10-24 10:05:50 +0700

    x

--- src/modules/tasks/dto/update-task-status.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsEnum } from 'class-validator';
-import { TaskStatus } from '@prisma/client';
-
-export class UpdateTaskStatusDto {
-  @ApiProperty({ enum: TaskStatus, example: TaskStatus.DONE })
-  @IsEnum(TaskStatus)
-  status: TaskStatus;
-}
